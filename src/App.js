108310761102f6458b6636e3d10f9ab55af6d175
ThuyTrang144 <thuytrangnguyen565@gmail.com>
2018-07-11 11:17:15 +0700

    work with button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,57 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Components/Header.js'
 import Intro from './Components/Intro.js'
+import Button from './Components/Button.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: 1,
+      status: true
+    }
+  }
+
+  handleChangeEvent = (name) => {
+    const value = this.state.value
+    this.setState({
+      value: name==='+' ? value+1 : value-1
+    })
+  }
+
+  handleChangeStatus = (name) => {
+    this.setState(preState => ({
+      status: !preState.status
+    }))
+  }
+  renderButton = () => {
+    const button = [{
+      name: '+',
+      onClick: (name) => this.handleChangeEvent(name)
+    }, {
+      name: '-',
+      onClick: (name) => this.handleChangeEvent(name)
+    },{
+      name:  this.state.status.toString(),
+      onClick: (name) => this.handleChangeStatus(name)
+    }]
+    return (
+      button.map((btn, index) => (
+        <Button
+          key={index}
+          name={btn.name}
+          onClick={btn.onClick}
+         />
+      ))
+    )
+  }
   render() {
     return (
       <div className="App">
         <Header />
         <Intro />
+        {this.renderButton()}
+        <h2>{this.state.value}</h2>
       </div>
     );
   }
---
+++ src/Components/Number.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Number extends Component {
+  render() {
+    return (
+      <div>
+        <button>
+      </div>
+    )
+  }
+}
+export default Number
