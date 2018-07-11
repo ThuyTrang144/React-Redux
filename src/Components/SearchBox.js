a61d48c9bcf9e6a707e1179de07df674428edd78
ThuyTrang144 <thuytrangnguyen565@gmail.com>
2018-07-11 20:08:45 +0700

    render rating and search

---
+++ src/Components/SearchBox.js
@@ -0,0 +1,31 @@
+import React , {Component} from 'react'
+
+class SearchBox extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchText: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      searchText: event.target.value
+    })
+    this.props.updateSearch(event.target.value)
+  }
+
+  render () {
+    return (
+      <input
+        type="text"
+        placeholder="Search product"
+        name = "searchText"
+        onChange={this.handleChange}
+        value={this.state.searchText}
+      />
+    )
+  }
+}
+
+export default SearchBox
